215faf05381513f0711d7bfa81061789bd7633aa
Shreyansh <shreyanshmarshal2@gmail.com>
2026-01-20 12:29:25 +0530

    coordinate compression

--- template.cpp
+++ template.cpp
@@ -23,51 +23,14 @@ signed main() {
     freopen("output.txt", "w", stdout);
     #endif
 
-    ll n,c;
-    cin>>n>>c;
-    vector<ll> start(n),end(n),val(n);
-    for(ll i=0;i<n;i++){
-        cin>>start[i]>>end[i]>>val[i];
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin>>n;
+        loop(i,1,n) cout<<i<<" ";
+        cout<<endl;
     }
 
-    set<ll> st;
-    for(ll i=0;i<n;i++){
-        st.insert(start[i]);
-        st.insert(end[i]+1);
-    }
-
-    map<ll,ll> mpp;
-    int idx= 0;
-
-    for(auto it:st){
-        mpp[it] = idx;
-        idx++;
-    }
-
-    vector<ll> diff(idx+1,0);
-    for(ll i=0;i<n;i++){
-        diff[mpp[start[i]]]+=val[i];
-        diff[mpp[end[i]+1]]-=val[i];
-    }
-
-    
-
-    vector<ll> days;
-    for(auto it:st){
-        days.push_back(it);
-    }
-
-    ll ans = 0;
-    ll curr = 0;
-
-    for(int i = 0; i < (int)days.size() - 1; i++){
-        curr += diff[i];
-        ll duration = days[i+1] - days[i];
-        ans += duration * min(curr, c);
-    }
-
-    cout << ans << endl;
-
-   
     return 0;
 }
